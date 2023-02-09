560d270fef53c5e9d743feabf768a94866d61a2f
Dustin Irvine <46619649+BasicExp@users.noreply.github.com>
2023-02-09 16:59:10 -0600

    Added initial NoMana system structures

---
+++ AdaptiveRPG/Character/Components/Abilities/NoManaAbility.cs
@@ -0,0 +1,10 @@
+﻿namespace AdaptiveRPG.Character.Components.Abilities
+{
+    public class NoManaAbility
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public AbilityConst.TARGET_TYPE TargetType { get; set; }
+        public AbilityConst.EFFECT_TYPE EffectType { get; set; }
+    }
+}
---
+++ AdaptiveRPG/Character/Systems/NoMana/Character.cs
@@ -0,0 +1,36 @@
+﻿using AdaptiveRPG.Character.Components.Equipment;
+using AdaptiveRPG.Character.Components.Leveling;
+using AdaptiveRPG.Character.Components.Stats;
+using AdaptiveRPG.Character.Components.CharacterClasses;
+
+namespace AdaptiveRPG.Character.Systems.NoMana
+{
+    public class Character
+    {
+        public readonly NoManaStats stats;
+        public readonly NoManaCharacterClass characterClass;
+        public readonly NoManaLeveling leveling;
+        public NoManaWeapon Weapon { get; set; }
+        public NoManaEquipment Armor { get; set; }
+        public NoManaEquipment Hat { get;  set; }
+        public NoManaEquipment Shoes { get; set; }
+
+        public int Attack
+        {
+            get
+            {
+                return stats.Attack + characterClass.Attack + Weapon.Attack + 
+                    Armor.Attack + Hat.Attack + Shoes.Attack;
+            }
+        }
+
+        public int Defense
+        {
+            get
+            {
+                return stats.Defense + characterClass.Defense + Weapon.Defense +
+                    Armor.Defense + Hat.Defense + Shoes.Defense;
+            }
+        }
+    }
+}
